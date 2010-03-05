3b233a1b5bbf0cf5fdfc73ef0620f19866872610
Lewis Oaten <sphyrz@gmail.com>
2010-03-05 12:21:48 +0000

    Updated paths in build file

--- build.php
+++ build.php
@@ -2,7 +2,6 @@
 function build() {
 	//File Lists
 	$out .= "<pre>" . `mv -f PRCO303/index.html ./` . "</pre>";
-	$out .= "<pre>" . `mv -f PRCO303/deploy.php ./` . "</pre>";
 	$out .= "<pre>" . `mv -fr PRCO303/mapserver/ ./` . "</pre>";
 	$out .= "<pre>" . `mv -fr PRCO303/scripts/ ./` . "</pre>";
 	return $out;
