11cc2bc5c268dc5026a3d0bf4b860d78aa1db51c
Lewis Oaten <sphyrz@gmail.com>
2010-03-05 12:36:02 +0000

    Modified build file to include permissions and removal of old files first.

--- build.php
+++ build.php
@@ -1,9 +1,17 @@
 <?php
 function build() {
-	//File Lists
+	//Old File Lists
+	$out .= "<pre>" . `rm -Rf index.html` . "</pre>";
+	$out .= "<pre>" . `rm -Rf scripts` . "</pre>";
+
+	$out .= "<pre>" . `rm -Rf mapserver` . "</pre>";
+
+	//New File Lists
 	$out .= "<pre>" . `mv -f PRCO303/index.html ./` . "</pre>";
-	$out .= "<pre>" . `mv -f PRCO303/mapserver/ ./` . "</pre>";
-	$out .= "<pre>" . `mv -f PRCO303/scripts/ ./` . "</pre>";
+	$out .= "<pre>" . `mv -f PRCO303/scripts ./` . "</pre>";
+
+	$out .= "<pre>" . `mv -f PRCO303/mapserver ./` . "</pre>";
+	$out .= "<pre>" . `chmod -R 777 mapserver/tmp` . "</pre>";
 	return $out;
 }
 ?>
