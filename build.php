ed6efa946cbdf28f01c24ff949580c205c14c88a
Lewis Oaten <sphyrz@gmail.com>
2010-03-05 12:29:07 +0000

    Removed invalid flag

--- build.php
+++ build.php
@@ -2,8 +2,8 @@
 function build() {
 	//File Lists
 	$out .= "<pre>" . `mv -f PRCO303/index.html ./` . "</pre>";
-	$out .= "<pre>" . `mv -fr PRCO303/mapserver/ ./` . "</pre>";
-	$out .= "<pre>" . `mv -fr PRCO303/scripts/ ./` . "</pre>";
+	$out .= "<pre>" . `mv -f PRCO303/mapserver/ ./` . "</pre>";
+	$out .= "<pre>" . `mv -f PRCO303/scripts/ ./` . "</pre>";
 	return $out;
 }
 ?>
